cf346b8118285bdf0f27756c2cd6f9686a099731
ParkSeoHai <anhhai282003@gmail.com>
2024-12-05 08:13:59 +0700

    update

---
+++ src/components/News/NewsItem.vue
@@ -0,0 +1,109 @@
+<script setup>
+import Breadcrumb from "@/components/Breadcrumb.vue";
+</script>
+
+<template>
+  <breadcrumb breadcrumb-active="Tin tức" />
+  <section id="template-news-item">
+    <div class="group-news-item">
+      <img
+        class="img-top"
+        src="https://cdn-media.sforum.vn/storage/app/media/trannghia/review-deal-smartphne-black-friday-2024.jpg"
+      />
+      <div class="group-content">
+        <button class="btn btn-news-category">Thị trường</button>
+        <h1 class="news-title">
+          Xiaomi Mi Band 9 Pro có tính năng gì mới? Ra mắt khi nào và giá bao nhiêu?
+        </h1>
+        <div class="group-infor__author">
+          <div>
+            <img
+              class="group-infor__author--avt"
+              src="https://cdn-media.sforum.vn/storage/app/media/Profile%20CTV/tran-nghia.jpg"
+            />
+            <div>
+              <p class="group-infor__author--name">Hải Nam</p>
+              <p class="group-infor__author--time">
+                <span>icon</span>
+                <span>Ngày đăng: 03/12/2024 - Cập nhật: 03/12/2024</span>
+              </p>
+            </div>
+          </div>
+        </div>
+        <p class="news-desc">
+          <b
+            >Các deal smartphone “hot” giảm sập sàn trong chương trình Black Friady 2024
+            liệu có giảm giá thực sự và thấp hơn các cửa hàng bán lẻ hay sàn TMĐT khác?
+            Cùng tìm hiểu qua bài viết dưới đây.</b
+          >
+          Sau nhiều ngày chờ đợi, Black Friday 2023 đã chính thức khởi động với rất nhiều
+          deal giảm giá flash sale theo khung giờ cùng hàng loạt các sản phẩm được giảm
+          giá trong suốt thời gian diễn ra chương trình. Trong bài viết dưới đây, chúng ta
+          sẽ thử so sánh mức giá của một số deal hot về điện thoại thông minh so với mức
+          giá ngày thường cũng như so với mặt bằng chung thị trường xem những deal này có
+          thực sự hot không nhé.
+        </p>
+        <div class="news-img">
+          <img
+            src="https://cdn-media.sforum.vn/storage/app/media/trannghia/Black-Friday-2024-CellphoneS.jpg"
+          />
+          <p class="news-img__text">Sale Black Friday 2024</p>
+        </div>
+        <div class="news-menu">
+          <div class="news-menu__header">
+            <p>Mục lục</p>
+            <p>Ẩn <span>icon</span></p>
+          </div>
+          <ul class="news-menu__body">
+            <li>
+              <a href="#"
+                >1. Một số deal hot về smartphone trong chương trình Black Friday 2024</a
+              >
+            </li>
+            <li>
+              <a href="#">2. Chương trình khuyến mãi Black Friday hấp dẫn</a>
+            </li>
+            <li>
+              <a href="#">3. Tạm kết</a>
+            </li>
+          </ul>
+        </div>
+        <div class="news-content">
+          <h2>
+            Một số deal hot về smartphone tại CellphoneS trong chương trình Black Friday
+            2024
+          </h2>
+          <p>
+            Dưới đây là một số điện thoại thông minh có giá giảm sâu nằm trong chương
+            trình Black Friday 2024 của CellphoneS.
+          </p>
+          <b>Tecno Camon 30S</b>
+          <ul>
+            <li>Giá ngày thường: 4,790,000đ</li>
+            <li>Giá ngày thường: 4,790,000đ</li>
+          </ul>
+          <p>
+            Tecno Camon 30S là một trong những smartphone tầm trung chất lượng vừa cập bến
+            thị trường Việt Nam cách đây không lâu. Sản phẩm này được trang bị màn hình
+            OLED cong 6.78 inch Full-HD+ 120Hz cao cấp cho chất lượng hiển thị tuyệt vời.
+            Bên trong, máy sử dụng chip MediaTek Helio G100, đi kèm RAM 8GB, bộ nhớ trong
+            256GB, hỗ trợ kháng nước chống bụi chuẩn IP53 và màn hình cảm ứng có thể hoạt
+            động khi tay bị ướt.
+          </p>
+          <div class="news-img">
+            <img
+              src="https://cdn-media.sforum.vn/storage/app/media/trannghia/Black-Friday-2024-CellphoneS.jpg"
+            />
+            <p class="news-img__text">Sale Black Friday 2024</p>
+          </div>
+          <p>
+            Điện thoại này hiện đang được bán trên một số nhà bán lẻ khác nhưng mức giá
+            trung bình cao hơn một chút so với giá sale trong chương trình Black Friday
+            2024 của CellphoneS. Do đó, đây là deal hot chất lượng vì người dùng không thể
+            nào tìm nơi bán giá thấp hơn.
+          </p>
+        </div>
+      </div>
+    </div>
+  </section>
+</template>
---
+++ src/views/News.vue
@@ -0,0 +1,13 @@
+<script setup>
+import NewsItem from "@/components/News/NewsItem.vue";
+</script>
+<template>
+  <section id="section-news">
+    <div class="row">
+      <div class="col-2">Navigation</div>
+      <div class="col-10">
+        <news-item></news-item>
+      </div>
+    </div>
+  </section>
+</template>
